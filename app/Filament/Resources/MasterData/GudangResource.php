63676190581ac7ee65f07e5d85a081e30acb6876
geprakoso <galihdanang48@live.com>
2025-11-14 09:28:09 +0700

    Gudang

---
+++ app/Filament/Resources/MasterData/GudangResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GudangResource\Pages;
+use App\Filament\Resources\GudangResource\RelationManagers;
+use App\Models\Gudang;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GudangResource extends Resource
+{
+    protected static ?string $model = Gudang::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGudangs::route('/'),
+            'create' => Pages\CreateGudang::route('/create'),
+            'view' => Pages\ViewGudang::route('/{record}'),
+            'edit' => Pages\EditGudang::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/MasterData/GudangResource/Pages/ViewGudang.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\GudangResource\Pages;
+
+use App\Filament\Resources\GudangResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewGudang extends ViewRecord
+{
+    protected static string $resource = GudangResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\EditAction::make(),
+        ];
+    }
+}
