3185e4aaeaf828b02b72b98cf63e728087719e5a
alamulhuda <hudachapter1998@gmail.com>
2025-11-15 13:39:06 +0700

    create registration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->registration()
             ->login()
             ->colors([
                 'primary' => Color::Amber,
