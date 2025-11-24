93e7ff2b1b1c6771450c625c53bf653e18028985
alamul huda <hudachapter1998@gmail.com>
2025-11-24 19:00:14 +0700

    sidebarcollapondekstop

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->registration()
+            ->sidebarCollapsibleOnDesktop()
             ->databaseNotifications()
             ->databaseNotificationsPolling('10s')
             ->login()
